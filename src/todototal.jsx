78e0aab46e894cf541384f917f953559bab59294
allayar99 <allayar99@gmail.com>
2026-01-22 21:28:53 +0900

    id

--- src/todototal.jsx
+++ src/todototal.jsx
@@ -1,60 +1,93 @@
-import React from "react"
-import styled from "styled-components"
+import React, { useState } from "react";
+import styled from "styled-components";
+// import React, { useState } from "react";
 
 const Todolist = () => {
-    // const [todo, setTodo] = useState([]);
-    // const [text, setText] = useTate("");
-    // console.log(text);
-    const addTodo = () => {
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("");
+  console.log(text);
+  const addTodo = (e) => {
+    e.preventDefaulte();
+    const value = text.trim();
+    if (!value) return;
 
-    }
-    return (
-        <Container>
-            <TodoWrapper>
-            <h1>Todo List</h1>
-            <Form>
-                <input type="text" placeholder="Todo lsit" />
-                <Button>Add</Button>
-            </Form>
-            <ul>
-                <li>Runnuing</li>
-            </ul>
-            </TodoWrapper>
-        </Container>
-    );
+    const newTodo = {
+      id: Date.now(),
+      text: value,
+      done: false,
+    };
+    setTodo((prev) => [newTodo, ...prev]);
+    setTodo("");
+  };
+//   console.log(todo);
+  return (
+    <Container>
+      <TodoWrapper>
+        <h1>Todo {todo.length} List</h1>
+        <Form onSubmit={addTodo}>
+          <input
+            type="text"
+            placeholder="Todo lsit"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <Button type="submit">Add</Button>
+        </Form>
+        <span>Total:</span>
+        {/* <ul>
+          <div style={{ display: "flex" }}>
+            <input type="checkbox" />
+            <li>Runnuing</li>
+            <button>delete</button>
+          </div>
+        </ul> */}
+        <ul>
+          {todo.map((value, index) => {
+            return (
+                <div style={{ display: "flex" }} key={value.id}>
+              <li key={value.id}>
+                {index} {value.text}{" "}
+              </li>
+              <button>delete</button>
+              </div>
+            );
+          })}
+        </ul>
+      </TodoWrapper>
+    </Container>
+  );
 };
 
 const Container = styled.div`
-display: flex;
-justify-content: center;
-
+  display: flex;
+  justify-content: center;
 `;
 
 const TodoWrapper = styled.div`
-text-align: center;
+  display: flex;
+  flex-direction: column;
 
-border: 1px solid lightgray;
-border-radius: 8px;
-width: 500px;
+  text-align: center;
+
+  border: 1px solid lightgray;
+  border-radius: 8px;
+  width: 500px;
+  padding: 20px;
 `;
 
 const Form = styled.form`
-display: flex;
-justify-content: center;
-
+  display: flex;
+  justify-content: center;
 `;
 
 const Button = styled.button`
-padding: 7px 20px;
-margin-left: 10px;
-
-`
+  padding: 7px 20px;
+  margin-left: 10px;
+`;
 
 const Input = styled.input`
-width: 250px;
-padding-left: 10px;
-`
-
-
+  width: 250px;
+  padding-left: 10px;
+`;
 
 export default Todolist;
